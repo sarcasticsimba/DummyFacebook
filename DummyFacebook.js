844f6f35c310a9e0d51f184ee743c2667e71df54
Vignesh Kalidas <vigneshk14@me.com>
2014-12-27 09:57:58 -0800

    Initial commit: Adding all files

---
+++ DummyFacebook.js
@@ -0,0 +1,5 @@
+(function()
+{
+    document.body.style.backgroundColor = "black";
+    document.body.innerHTML = '<p style="text-align: center; color: #fff; font-size: 100px; margin: 150px">YOUR TIME IS BETTER SPENT ELSEWHERE & HIGH SCHOOL FRIENDS: STILL IDIOTS</p>';
+})();
